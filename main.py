1d8ef2598d83b51883978d2f8367b9311c186cbe
Sasi Kiranmayi Pasupuleti <sasikiranmayi.pasupuleti@gmail.com>
2025-12-01 00:01:07 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import asyncio
-import os
 
 from dotenv import load_dotenv
+
 from google.adk.runners import Runner
-from google.adk.sessions import DatabaseSessionService
+from google.genai import types
 
 from investment_planner.agent import root_agent
 from investment_planner.sessions.session_manager import session_service
@@ -11,20 +11,40 @@ from investment_planner.observability.logging_config import logger
 
 load_dotenv()
 
+USER_ID = "default_user"
+SESSION_ID = "default_session"
+APP_NAME = "investment_planner"
+
+session = session_service.create_session(
+    app_name=APP_NAME,
+    user_id=USER_ID,
+    session_id=SESSION_ID
+)
+
 
 async def main():
 
     runner = Runner(
         agent=root_agent,
+        app_name=APP_NAME,
         session_service=session_service,
     )
 
     logger.info("🚀 Starting Multi-Agent Investment Planner (Runner mode)")
     logger.info(
         "Type your first message (e.g., 'Hi, I want to plan my investments').")
-
-    # run_debug opens an interactive REPL in the terminal
-    await runner.run_debug()
+    replies = []
+    user_input = input("You: ")
+    user_msg = types.Content(role="user", parts=[types.Part(text=user_input)])
+    # Run the agent with the runner
+    for event in runner.run(
+            user_id=USER_ID,
+            session_id=SESSION_ID,
+            new_message=user_msg
+    ):
+        if event.content:
+            replies.append(event.content.parts[0].text)
+            print("Agent:", event.content.parts[0].text)
 
 
 if __name__ == "__main__":
